da88e400352c57daa339ca9c262fa87793e877f6
Leandro Sousa <1190800@isep.ipp.pt>
2023-03-06 11:20:57 +0000

    Adds Flutter Project

---
+++ Flutter/app1/android/app/src/main/kotlin/com/kaizen/app1/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.kaizen.app1
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ Flutter/app1/lib/ListItem.dart
@@ -0,0 +1,30 @@
+class HeadingItem implements ListItem {
+  final String heading;
+
+  HeadingItem(this.heading);
+
+  @override
+  Widget buildTitle(BuildContext context) {
+    return Text(
+      heading,
+      style: Theme.of(context).textTheme.headlineSmall,
+    );
+  }
+
+  @override
+  Widget buildSubtitle(BuildContext context) => const SizedBox.shrink();
+}
+
+/// A ListItem that contains data to display a message.
+class MessageItem implements ListItem {
+  final String sender;
+  final String body;
+
+  MessageItem(this.sender, this.body);
+
+  @override
+  Widget buildTitle(BuildContext context) => Text(sender);
+
+  @override
+  Widget buildSubtitle(BuildContext context) => Text(body);
+}
